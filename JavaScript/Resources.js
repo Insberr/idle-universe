ed89ed6a1a6806453d570231c02c25e86ca98c48
SpiderGamin <jaytaygame@gmail.com>
2020-03-09 00:19:37 +0000

    Update Resources.js

--- JavaScript/Resources.js
+++ JavaScript/Resources.js
@@ -1,18 +1,18 @@
 // Wood
 
-var food = 1;
+//var food = 1;
 
 function gatherWood() {
-    var life = food + 1;
-    var food = life;
-   // localStorage.setItem("IncrementalWood", number(wood));
-    document.getElementById('woodamount').value = food;
+    //var life = food + 1;
+    //var food = life;
+    //localStorage.setItem("IncrementalWood", number(wood));
+    document.getElementById('woodamount').value = ++wood;
 }
 
 // Stone
 
-var i = 0;
+var stone = 0;
 
 function gatherStone() {
-    document.getElementById('stoneamount').value = ++i;
+    document.getElementById('stoneamount').value = ++stone;
 }
