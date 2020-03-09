d878788bbaae2f5b95c0521f68b6038ec623b269
SpiderGamin <jaytaygame@gmail.com>
2020-03-09 16:43:01 +0000

    Added more variables

--- JavaScript/default-var.js
+++ JavaScript/default-var.js
@@ -1,3 +1,3 @@
 // Resources
-var wood = 0;
-var stone = 0;
+var wood = 0, maxwood = 50, woodonload = wood + "/" + maxwood;
+var stone = 0, maxstone = 50, stoneonload = stone + "/" + maxstone, lockstone = true;
