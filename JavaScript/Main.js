c6861a47c88189b4beae5686bf04fa1f3bd5a381
SpiderGamin <jaytaygame@gmail.com>
2020-07-24 14:24:42 -0700

    idk

--- JavaScript/Main.js
+++
@@ -1,7 +0,0 @@
-function onLoad() {
-    document.getElementById("woodamount").innerHTML = woodonload;
-    document.getElementById('stoneamount').innerHTML = stoneonload;
-    document.getElementById('stone').style.display = "none";
-}
-
-onLoad();
--- JavaScript/default-var.js
+++
@@ -1,3 +0,0 @@
-// Resources
-var wood = 0, maxwood = 50, woodonload = wood + "/" + maxwood;
-var stone = 0, maxstone = 50, stoneonload = stone + "/" + maxstone, lockstone = true;
